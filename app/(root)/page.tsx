97d509b37ae494f5268fff893aee3997134b0493
Tinotenda Fred <fredtinotenda3@gail.com>
2025-01-12 17:28:19 +0200

    2. implement: Home Data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,19 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 import { Book } from "lucide-react";
 import React from "react";
 
 const Home = async () => (
   <>
-    <BookOverview />
-    <BookList />
+    <BookOverview totalCopies={0} availableCopies={0} {...sampleBooks[0]} />
+
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 
