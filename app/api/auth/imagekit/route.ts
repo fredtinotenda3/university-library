ac9b2d0f6d777c0f637b475ec6ae9075bb40539b
Tinotenda Fred <fredtinotenda3@gail.com>
2025-01-13 16:25:04 +0200

    4d

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -2,27 +2,13 @@ import ImageKit from "imagekit";
 import config from "@/lib/config";
 import { NextResponse } from "next/server";
 
-// Debugging: log the values of publicKey, privateKey, and urlEndpoint before initialization
 const {
   env: {
     imagekit: { publicKey, privateKey, urlEndpoint },
   },
 } = config;
 
-console.log("Public Key from config:", publicKey); // Log publicKey to check its value
-console.log("Private Key from config:", privateKey); // Log privateKey to check its value
-console.log("URL Endpoint from config:", urlEndpoint); // Log URL Endpoint to check its value
-
-if (!publicKey) {
-  console.error("Error: publicKey is not defined.");
-}
-
-// Initialize ImageKit instance
-const imagekit = new ImageKit({
-  publicKey,
-  privateKey,
-  urlEndpoint,
-});
+const imagekit = new ImageKit({ publicKey, privateKey, urlEndpoint });
 
 export async function GET() {
   return NextResponse.json(imagekit.getAuthenticationParameters());
