8e215123b2f9df1af52873f97d8986269c351065
Tinotenda Fred <fredtinotenda3@gail.com>
2025-01-14 16:08:52 +0200

    implement:  Neon and Drizzle Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,19 +2,25 @@ import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 import { Book } from "lucide-react";
 import React from "react";
 
-const Home = async () => (
-  <>
-    <BookOverview totalCopies={0} availableCopies={0} {...sampleBooks[0]} />
+const Home = async () => {
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result, null, 2));
+  return (
+    <>
+      <BookOverview totalCopies={0} availableCopies={0} {...sampleBooks[0]} />
 
-    <BookList
-      title="Latest Books"
-      books={sampleBooks}
-      containerClassName="mt-28"
-    />
-  </>
-);
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
 
 export default Home;
