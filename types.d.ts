9f69bd70f80acb5b0e64454b4da3d7daa7c99329
Tinotenda Fred <fredtinotenda3@gail.com>
2025-01-18 15:57:47 +0200

    implement: Create book action

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,5 @@
 interface Book {
-  id: number;
+  id: string;
   title: string;
   author: string;
   genre: string;
@@ -7,12 +7,11 @@ interface Book {
   totalCopies: number;
   availableCopies: number;
   description: string;
-  color: string;
-  cover: string;
-  video: string;
+  coverColor: string;
+  coverUrl: string;
+  videoUrl: string;
   summary: string;
-  isLoanedBook?: boolean;
-  coverImage?: string;
+  createdAt: Date | null;
 }
 
 interface AuthCredentials {
@@ -21,4 +20,22 @@ interface AuthCredentials {
   password: string;
   universityId: number;
   universityCard: string;
+}
+
+interface BookParams {
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  coverUrl: string;
+  coverColor: string;
+  description: string;
+  totalCopies: number;
+  videoUrl: string;
+  summary: string;
+}
+
+interface BorrowBookParams {
+  bookId: string;
+  userId: string;
 }
